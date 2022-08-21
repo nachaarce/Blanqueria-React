ed7a64a863a45882268dd4d42e1a193c2d92cac4
Nazarena Arce <nazarena.arce8@gmail.com>
2022-08-20 21:23:57 -0300

    se incorpora navbar a la app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+      <header>
+        <NavBar />
+      </header>
+
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-items">
+                <li className="nav-item">Blanqueria MyM</li>
+                <li className="nav-item">Hogar</li>
+                <li className="nav-item">Baño</li>
+                <li className="nav-item">Profesional</li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar;
