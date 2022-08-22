bad1d62ab8a5f5b104093a99eca3d63fde7359d2
Nazarena Arce <nazarena.arce8@gmail.com>
2022-08-22 19:38:27 -0300

    se incorpora componente itemlist con su estilo integrado, modificacion

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,5 +1,4 @@
 const ItemListContainer = (greeting) =>  {
-    console.log (greeting.list)
     return (
     <ul className="navbar-items">
         <li style={{fontStyle: greeting.estilo}}> {greeting.list} </li>
